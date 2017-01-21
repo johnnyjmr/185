a432814688c58f32457f60415e5eb26521ea57f5
Harshit Prasad <harshit98@users.noreply.github.com>
2017-01-21 12:34:19 +0530

    Created main.js file
    I have created this new main.js file, which will be used in your alert.html file.

---
+++ test.js
@@ -0,0 +1,9 @@
+// This the javascript which will be used to display alert in any web-browser
+var age = confirm("Enter your age!");
+
+if (age > 18) {
+  alert("Welcome to Github!");
+}
+else {
+  prompt("You're not eligible to use Github yet!");
+}
