5c9063681b112a154dd812afc0629260d62e6fa6
Harshit Prasad <harshit98@users.noreply.github.com>
2017-01-21 12:38:01 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
 // This the javascript which will be used to display alert in any web-browser
-var age = confirm("Enter your age!");
+function myFunction() {
+  var age = confirm("Enter your age!");
 
-if (age > 18) {
-  alert("Welcome to Github!");
-}
-else {
-  prompt("You're not eligible to use Github yet!");
-}
+  if (age > 18) {
+    alert("Welcome to Github!");
+  }
+  else {
+    prompt("You're not eligible to use Github yet!");
+  }
+};
+
