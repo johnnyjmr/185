39e0935617cf7ebd3eba9cea1a0a82ad231857a6
Johnny Mendoza Ramirez <jhonnyjmr@Johnny-Mendoza-Ramirezs-MacBook.local>
2017-01-20 23:41:21 -0700

    i did it. i finish it

---
+++ Code/Assigment1/consoleHelloWorld.js
@@ -0,0 +1,4 @@
+/**
+ * Created by jhonnyjmr on 1/20/17.
+ */
+console.log("Hello World");
--- Code/TRYES/JS_TEST.js
+++ Code/TRYES/JS_TEST.js
@@ -3,11 +3,23 @@
  */
 
 
-function checck(bb){
+function checck(bb) {
     bb.length
-
-
-
-
+}
+/*
+ parseInt(num); // default way
+ parseInt(num, 10); // parseInt with radix (decimal)
+ parseFloat(num) // floating point
+ Number(num); // Number constructor
+ */
+function numberConvertion(aa) {
+    var number = aa;
+    document.writeln(aa + " converted to number with parseInt(num); is " + String(parseInt(number))+"<br>");
+    document.writeln(aa + " converted to number with parseInt(num, 10); is " + String(parseInt(number, 10))+"<br>");
+    document.writeln(aa+ " converted to number with parseFloat(num); is "+String(parseFloat(number))+"<br>");
+    document.writeln(aa+ " converted to number with Number(num); is "+String(Number(number))+"<br>"+"<br>");
 
 }
+
+
+
